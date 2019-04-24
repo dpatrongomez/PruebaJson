654a07552c45292ad38246222448e7007d09839a
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2019-04-24 18:26:17 +0200

    Add Code

--- app/src/main/java/com/dpatrongomez/pruebajson/MainActivity.java
+++ app/src/main/java/com/dpatrongomez/pruebajson/MainActivity.java
@@ -2,12 +2,71 @@ package com.dpatrongomez.pruebajson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTextViewResult.append("");
+                jsonParse();
+            }
+        });
+    }
+
+    private void jsonParse() {
+
+        String url = "http://urbanos.guadalajara.es:8080/SSIIMovilWS/ws/cons/tiemposParada.json?codParada=123";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("tiempos");
+
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject tiempos = jsonArray.getJSONObject(i);
+
+                        int Minutos = tiempos.getInt("minutos");
+
+                        mTextViewResult.append(String.valueOf(Minutos) + " minutos");
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
     }
 }
